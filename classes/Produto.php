3374dc84afe10d507c83ae4f17248ff542366303
Marcio <166270115+cordeiromarcio@users.noreply.github.com>
2026-01-19 20:10:07 -0300

    Inicio Composição

--- classes/Produto.php
+++ classes/Produto.php
@@ -5,12 +5,18 @@ class Produto{
     private $estoque;
     private $preco;
     private $fabricante;
+    private $caracteristicas;
 
     public function __construct($descricao, $estoque, $preco)
     {
         $this->descricao = $descricao;
         $this->estoque = $estoque;
         $this->preco = $preco;
+    }
+
+    public function addCaracteristica()
+    {
+        
     }
 
     public function getDescricao()
