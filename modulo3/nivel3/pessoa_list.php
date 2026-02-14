5c942febe65a545c5ef842dc5d1cdc06d1934e2f
Marcio <166270115+cordeiromarcio@users.noreply.github.com>
2026-02-13 21:18:04 -0300

    Listagem com separação do PHP e HTML

--- modulo3/nivel3/pessoa_list.php
+++ modulo3/nivel3/pessoa_list.php
@@ -1,27 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listagem de Pessoas</title>
-    <link rel="stylesheet" href="css/list.css" type="text/css" media="screen" />
-</head>
-
-<body>
-    <table border="1">
-        <thead>
-            <tr>
-                <td></td>
-                <td></td>
-                <td>Id</td>
-                <td>Nome</td>
-                <td>Endereço</td>
-                <td>Bairro</td>
-                <td>Telefone</td>
-            </tr>
-        </thead>
-        <tbody>
             <?php
             //cria conexão com o banco de dados
             $conn = mysqli_connect('127.0.0.1', 'root', '1234', 'myapp');
@@ -34,56 +10,25 @@
 
             $result = mysqli_query($conn, 'SELECT * from pessoa ORDER BY id');
 
+            $items = '';
+
             while ($row = mysqli_fetch_assoc($result)) {
-                $id = $row['id'];
-                $nome = $row['nome'];
-                $endereco = $row['endereco'];
-                $bairro = $row['bairro'];
-                $telefone = $row['telefone'];
-                $email = $row['email'];
-                $id_cidade = $row['id_cidade'];
 
-                print '<tr>';
+                $item = file_get_contents('html/item.html');
+                $item = str_replace('{id}', $row['id'], $item);
+                $item = str_replace('{nome}', $row['nome'], $item);
+                $item = str_replace('{endereco}', $row['endereco'], $item);
+                $item = str_replace('{bairro}', $row['bairro'], $item);
+                $item = str_replace('{telefone}', $row['telefone'], $item);
 
-                //alteração de melhoria delete-list insert-edit
-                /*------------------------Como era no nível 1----------------------
-                        print "<td> <a href= 'pessoa_form_edit.php?id={$id}'>
-                                    <img src='images/edit.svg' style='width:17px'>
-                                    </a> </td>";
-
-                       
-                        print "<td> <a href= 'pessoa_delete.php?id={$id}'>
-                                    <img src='images/delete.svg' style='width:17px'>
-                                    </a> </td>";
-                        
-                        -----------------------------------------------------------------*/
-                print "<td> <a href= 'pessoa_form.php?action=edit&id={$id}'>
-                                    <img src='images/edit.svg' style='width:17px'>
-                                    </a> </td>";
-
-                print "<td> <a href= 'pessoa_list.php?action=delete&id={$id}'>
-                                    <img src='images/delete.svg' style='width:17px'>
-                                    </a> </td>";
-
-                print "<td> {$id} </td>";
-                print "<td> {$nome} </td>";
-                print "<td> {$endereco} </td>";
-                print "<td> {$bairro} </td>";
-                print "<td> {$telefone} </td>";
-                print '</tr>';
+                //acumula linhas de ítens da tabela
+                $items .= $item;
             }
+
+
+            $list = file_get_contents('html/list.html');
+            $list =str_replace('{items}', $items, $list);
+            print $list;
+
             ?>
-        </tbody>
-
-    </table>
-    <!-- Como era nível 1-------------------------------------------
-    <button onclick="window.location= 'pessoa_form_insert.php'">
-        <img src="images/insert.svg" style="width: 17px">Inserir
-    </button>
-    --------------------------------------------------------------- -->
-    <button onclick="window.location= 'pessoa_form.php'">
-        <img src="images/insert.svg" style="width: 17px">Inserir
-    </button>
-</body>
-
-</html>
+        
