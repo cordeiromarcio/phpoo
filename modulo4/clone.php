666b37baa3f01f441b55bce570646e29e052b347
Marcio <166270115+cordeiromarcio@users.noreply.github.com>
2026-02-17 09:21:58 -0300

    Método clone

---
+++ modulo4/clone.php
@@ -0,0 +1,25 @@
+<?php
+class Titulo
+{
+    public $codigo, $dt_vencimento, $valor, $juros, $multa;
+    
+    // Método clone pode ser utilizado para criar novo registro limpar um determinado dado
+    public function __clone()
+    {
+        $this->codigo = null;
+    }
+}
+
+$titulo = new Titulo;
+$titulo->codigo = 1;
+$titulo->dt_vencimento = '2018-10-10';
+$titulo->valor = 100;
+$titulo->juros = 0.1;
+$titulo->multa = 1;
+
+$titulo2 = clone $titulo;
+$titulo2->valor = 200;
+
+echo '<pre>';
+var_dump($titulo);
+var_dump($titulo2);
