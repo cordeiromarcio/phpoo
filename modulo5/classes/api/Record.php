dee095ba3d16ae627be086c0801ad7017c028a4b
Marcio <166270115+cordeiromarcio@users.noreply.github.com>
2026-07-28 20:06:35 -0300

    Update Record

--- modulo5/classes/api/Record.php
+++ modulo5/classes/api/Record.php
@@ -6,6 +6,58 @@ abstract class Record
 
     public function __set($prop, $value)
     {
-        $this->data[$prop] = $value;
+        if ($value === null) {
+            unset($this->data[$prop]);
+        }
+        else {
+            $this->data[$prop] = $value;
+        };
+    }
+
+    public function __get($prop)
+    {
+        if (isset($this->data[$prop]))
+        {
+            return $this->data[$prop];
+        }
+        
+    }
+
+    public function __isset($prop)
+    {
+        return isset($this->data[$prop]);
+    }
+
+    public function __clone()
+    {
+        unset($this->data['id']);
+    }
+
+    public function fromArray($data)
+    {
+        $this->data = $data;
+    }
+
+    public function toArray()
+    {
+        return $this->data;
+    }
+
+    public function getEntity()
+    {
+        return get_class($this);
     }
 }
+
+$produto = new Produto;
+
+$produto = new Produto;
+$produto->id = 1;
+$produto->nome = 'Cafe';
+
+$produto2 = clone $produto;
+
+print $produto->nome;
+
+if (isset($produto->nome));
+
